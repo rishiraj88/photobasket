3e2d1418155ccdddc545496c8bcc2fd85ec2e223
Rishi Raj <rishiraj88+github@gmail.com>
2024-07-08 23:14:09 +0200

    Add search box and recommendation buttons to App view (Frontend).

--- photobasket/src/App.tsx
+++ photobasket/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
+import { Form } from 'react-bootstrap'
 
 const API_BASE_URL = "https://api.unsplash.com/search/photos?"
 const CLIENT_ID = import.meta.env.VITE_API_KEY
@@ -9,7 +10,25 @@ const CLIENT_ID = import.meta.env.VITE_API_KEY
 function App() {
 
   return (
-  )
+    <div className='container'>
+      <h1 className="title">Photo Bucket</h1>
+
+      <div className="search-section">
+        <Form onSubmit={handleSearchSubmit}>
+          <Form.Control className='search-input' type="search" placeholder='What may we present?' ref={searchControl} />
+        </Form>
+      </div>
+
+      <div className="recommendation">
+        <div onClick={() => handleSelection("europe")}>europe</div>
+        <div onClick={() => handleSelection("apartment")}>apartment</div>
+        <div onClick={() => handleSelection("computer")}>computer</div>
+        <div onClick={() => handleSelection("design")}>design</div>
+        <div onClick={() => handleSelection("bodyart")}>bodyart</div>
+        <div onClick={() => handleSelection("people")}>people</div>
+      </div>
+
+    </div >)
 }
 
 export default App
